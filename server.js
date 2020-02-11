8d118e86d21017b043fd02cd7c16faf11f7893d0
Afolabi Marvelous <afosmarvel@gmail.com>
2020-02-10 16:05:24 -0800

    Changes made

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express') ;
 
 const app = express() ;
 
-const port = 3030 ;
+
+const port = process.env.PORT || 3030 ;
 
 app.use(express.static(__dirname + '/client')) ;
 
