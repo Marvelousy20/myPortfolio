701551351ce948839cf7cdb5c1212f73de45d929
Afolabi Marvelous <afosmarvel@gmail.com>
2020-04-29 14:33:46 -0700

    changes made

---
+++ client/js/app.js
@@ -0,0 +1,48 @@
+// function display() {
+//     // let myInfo = document.querySelector('ul') ;
+//     // if(myInfo.style.display === 'none') {
+//     //     myInfo.style.display = 'block' 
+//     // }
+//     // else{
+//     //     myInfo.style.display = 'none'
+//     // }
+
+//     let myInfo = document.querySelector('ul') ;
+//     if(myInfo.style.display === 'none')
+//     myInfo.style.display = 'block'
+//     else
+//     myInfo.style.display = 'none'
+
+// }
+
+$(function() {
+    $('#disp').on('click', function() {
+        if($('.item').hasClass('active')) {
+            $('.item').removeClass('active')
+        }
+        else{
+            $('.item').addClass('active') 
+        }
+    })
+})
+
+$(function () {
+    $('.read').on('click', function() {
+        if($('.hide-at-first').hasClass('add')) {
+            $('.hide-at-first').removeClass('add')
+        }
+        else{
+            $('.hide-at-first').addClass('add')
+            $('.control').addClass('add-also')
+        }
+    })
+})
+
+$(function(){
+    $('.less').on('click', function() {
+        if($('.hide-at-first').hasClass('add')) {
+            $('.hide-at-first').removeClass('add')
+            $('.control').removeClass('add-also')
+        }
+    })
+})
--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,12 +1,12 @@
-function displayImage() {
-    let display = document.getElementById("display")
-    if(display.style.display == 'none'){
-        display.style.display = 'block';
-    }
-    else {
-        display.style.display = 'none';
-    }
-}
+// function displayImage() {
+//     let display = document.getElementById("display")
+//     if(display.style.display == 'none'){
+//         display.style.display = 'block';
+//     }
+//     else {
+//         display.style.display = 'none';
+//     }
+// }
 
 let body = document.querySelector('html');
 let nav = document.querySelector('.navul');
@@ -26,16 +26,16 @@ let tl = new TimelineMax() ;
 
 
 
-tl.fromTo(slider, 1, {height: '100%'}, {height: '0%', ease: Power2.easeInOut})
-.fromTo(logo, 1, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut})
-.fromTo(nav, 1, {x: '-600%'}, {x: '0%', ease: Power2.easeInOut})
-.fromTo(one, 1, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut})
-.fromTo(two, 1.5, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut})
-.fromTo(three, 1, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut})
-.fromTo(four, 1, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut})
-.fromTo(languages, 1, {opacity: 0}, {opacity:1}, '-=1')
-.fromTo(languages, 1, {x:100}, {x:0}, '-=1')
-.to(languages, 2, {rotate: "+=360"})
+tl.fromTo(slider, .5, {height: '100%'}, {height: '0%', ease: Power2.easeInOut})
+.fromTo(logo, .5, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut})
+.fromTo(nav, .5, {x: '-600%'}, {x: '0%', ease: Power2.easeInOut})
+.fromTo(one, .5, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut})
+.fromTo(two, .5, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut})
+.fromTo(three, .5, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut})
+.fromTo(four, .5, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut})
+.fromTo(languages, .5, {opacity: 0}, {opacity:1}, '-=.5')
+.fromTo(languages, 1, {x:100}, {x:0}, '-=.5')
+.to(languages, 1, {rotate: "+=360"}) ;
 
 
 // .to(languages, {duration: 3, rotation: 360}) 
@@ -47,3 +47,4 @@ tl.fromTo(slider, 1, {height: '100%'}, {height: '0%', ease: Power2.easeInOut})
 // .fromTo(five, 1, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut})
 
 //.fromTo(pad, 1, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut})
+
